525842c79b017fb825d3a60ca1f87d752e5fdd53
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-02 22:13:56 +0300

    Add LeadInterface->getId(): string;

--- src/Contract/LeadInterface.php
+++ src/Contract/LeadInterface.php
@@ -4,6 +4,7 @@ namespace Likemusic\YandexFleetTaxi\LeadRepository\Contract;
 
 interface LeadInterface
 {
+    public function getId(): string;
     public function getDriverPostData(): array;
     public function getCarPostData(): array;
 }
