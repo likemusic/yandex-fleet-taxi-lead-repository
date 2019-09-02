63e3ea95b864bd3b0483ff5bfeab3ee53d723f89
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-02 22:18:34 +0300

    Add $statusMessage param to LeadInterface->updateLeadStatus(): string;

--- src/Contract/LeadRepositoryInterface.php
+++ src/Contract/LeadRepositoryInterface.php
@@ -5,5 +5,5 @@ namespace Likemusic\YandexFleetTaxi\LeadRepository\Contract;
 interface LeadRepositoryInterface
 {
     public function getNewLeads();
-    public function updateLeadStatus(string $leadId, string $leadStatus);
+    public function updateLeadStatus(string $leadId, string $leadStatus, string $statusMessage = null);
 }
