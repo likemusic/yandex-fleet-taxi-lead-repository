02e6c208cf0c8844bb75b09e5f500ab6842f0659
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-03 12:15:09 +0300

    Add Lead class.

---
+++ src/Lead.php
@@ -0,0 +1,46 @@
+<?php
+
+use Likemusic\YandexFleetTaxi\LeadRepository\Contract\LeadInterface;
+
+class Lead implements LeadInterface
+{
+    private $id;
+    private $driverPostData;
+    private $carPostData;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getDriverPostData(): array
+    {
+        return $this->driverPostData;
+    }
+
+    public function getCarPostData(): array
+    {
+        return $this->carPostData;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function setDriverPostData(array $driverPostData): self
+    {
+        $this->driverPostData = $driverPostData;
+
+        return $this;
+    }
+
+    public function setCarPostData(array $carPostData): self
+    {
+        $this->carPostData = $carPostData;
+
+        return $this;
+    }
+}
